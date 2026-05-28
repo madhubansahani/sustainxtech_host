f5589c40653dfa436ffcc88c54af579f5ba231e9
madhubansahani <107603778+madhubansahani@users.noreply.github.com>
2026-05-28 14:19:41 +0530

    done new deploy

---
+++ .dart_tool/flutter_build/c72c6e6149056c379540743392607228/web_plugin_registrant.dart
@@ -0,0 +1,54 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:agora_rtc_engine/agora_rtc_engine_web.dart';
+import 'package:device_info_plus_web/device_info_plus_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:flutter_branch_sdk/src/flutter_branch_sdk_web.dart';
+import 'package:flutter_dropzone_web/flutter_dropzone_plugin.dart';
+import 'package:flutter_facebook_auth_web/flutter_facebook_auth_web.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:image_cropper_for_web/image_cropper_for_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:libphonenumber_web/libphonenumber_web.dart';
+import 'package:location_web/location_web.dart';
+import 'package:package_info_plus_web/package_info_plus_web.dart';
+import 'package:printing/printing_web.dart';
+import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AgoraRtcEngineWeb.registerWith(registrar);
+  DeviceInfoPlusPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FlutterBranchSdkWeb.registerWith(registrar);
+  FlutterDropzonePlugin.registerWith(registrar);
+  FlutterFacebookAuthPlugin.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  ImageCropperPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  LibPhoneNumberPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
+  PackageInfoPlugin.registerWith(registrar);
+  PrintingPlugin.registerWith(registrar);
+  SignInWithApplePlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  WakelockWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
